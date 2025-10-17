f207722a1755eac65958bac5a31df47c08d2b6b8
Harpreet Sahota <sahota.singh.harpreet@gmail.com>
2025-10-17 14:56:17 -0500

    add init

---
+++ __init__.py
@@ -0,0 +1,62 @@
+import logging
+import os
+
+from huggingface_hub import snapshot_download
+from fiftyone.operators import types
+
+from .zoo import BiModernVBertModel, BiModernVBertConfig 
+
+logger = logging.getLogger(__name__)
+
+def download_model(model_name, model_path):
+    """Downloads the model.
+
+    Args:
+        model_name: the name of the model to download, as declared by the
+            ``base_name`` and optional ``version`` fields of the manifest
+        model_path: the absolute filename or directory to which to download the
+            model, as declared by the ``base_filename`` field of the manifest
+    """
+    
+    snapshot_download(repo_id=model_name, local_dir=model_path)
+
+def load_model(model_name, model_path, **kwargs):
+    """Loads the model.
+
+    Args:
+        model_name: the name of the model to load, as declared by the
+            ``base_name`` and optional ``version`` fields of the manifest
+        model_path: the absolute filename or directory to which the model was
+            donwloaded, as declared by the ``base_filename`` field of the
+            manifest
+        **kwargs: optional keyword arguments that configure how the model
+            is loaded
+
+    Returns:
+        a :class:`fiftyone.core.models.Model`
+    """
+
+    # Start with base configuration
+    config_dict = {
+        "model_path": model_path,
+    }
+    
+    #Merge all kwargs into config_dict
+    config_dict.update(kwargs)
+    
+    # Create config and model
+    config = BiModernVBertConfig(config_dict)
+    return BiModernVBertModel(config)
+
+
+def resolve_input(model_name, ctx):
+    """Defines properties to collect the model's custom parameters.
+
+    Args:
+        model_name: the name of the model
+        ctx: an ExecutionContext
+
+    Returns:
+        a fiftyone.operators.types.Property
+    """
+    pass
